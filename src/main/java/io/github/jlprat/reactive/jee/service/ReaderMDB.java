f30ba65f3965b4b5ba2b5d9718f5a6ddf810d4d4
Josep Prat <jlprat@gmail.com>
2016-09-13 22:27:31 +0200

    skeleton for MDB

---
+++ src/main/java/io/github/jlprat/reactive/jee/service/ReaderMDB.java
@@ -0,0 +1,46 @@
+package io.github.jlprat.reactive.jee.service;
+
+import io.github.jlprat.reactive.jee.domain.Book;
+import io.github.jlprat.reactive.jee.domain.Reader;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.ejb.Stateless;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by @jlprat
+ */
+/*
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/BookLendingQueue")
+})
+@Stateless*/
+public class ReaderMDB implements MessageListener {
+
+    private static final Logger logger = Logger.getLogger(ReaderMDB.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void onMessage(Message message) {
+        final MapMessage msg = (MapMessage) message;
+        try {
+            Reader reader = em.find(Reader.class, msg.getString("readerId"));
+            Book book = em.find(Book.class, msg.getString("bookIsbn"));
+            reader.loanBook(book);
+            em.merge(reader);
+        } catch (JMSException e) {
+            logger.log(Level.SEVERE, "Error processing Message", e);
+        }
+    }
+}
