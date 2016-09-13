50a5fe0f82eada75d285d373e34fde7a5a8538a1
jpra <josep.prat@gameduell.de>
2016-09-13 12:24:30 +0200

    async server with futures

--- src/main/java/io/github/jlprat/reactive/jee/rest/AuthorWs.java
+++ src/main/java/io/github/jlprat/reactive/jee/rest/AuthorWs.java
@@ -1,7 +1,6 @@
 package io.github.jlprat.reactive.jee.rest;
 
 import io.github.jlprat.reactive.jee.domain.Author;
-import io.github.jlprat.reactive.jee.domain.Reader;
 import io.github.jlprat.reactive.jee.service.AuthorService;
 
 import javax.ejb.Stateless;
@@ -9,6 +8,12 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author @jlprat
@@ -16,6 +21,9 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("/users/authors")
 public class AuthorWs {
+
+    private static final Logger logger = Logger.getLogger(AuthorWs.class.getName());
+
 
     @Inject
     private AuthorService authorService;
@@ -40,7 +48,14 @@ public class AuthorWs {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createAuthor(@FormParam("name") final String name, @FormParam("surname") final String surname) {
-        final Author reader = authorService.createAuthor(name, surname);
-        return Response.ok(reader).build();
+        final Future<Author> authorFuture = authorService.createAuthor(name, surname);
+        try {
+            final Author author = authorFuture.get(2, TimeUnit.SECONDS);
+            return Response.ok(author).build();
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            logger.log(Level.SEVERE, "Error processing the future", e);
+            return Response.serverError().build();
+        }
+
     }
 }
--- src/main/java/io/github/jlprat/reactive/jee/service/AuthorService.java
+++ src/main/java/io/github/jlprat/reactive/jee/service/AuthorService.java
@@ -2,12 +2,15 @@ package io.github.jlprat.reactive.jee.service;
 
 import io.github.jlprat.reactive.jee.domain.Author;
 
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.Future;
 
 /**
  * @author @jlprat
@@ -22,10 +25,11 @@ public class AuthorService {
         return em.createNamedQuery(Author.ALL_AUTHORS, Author.class).getResultList();
     }
 
-    public Author createAuthor(final String name, final String surname) {
+    @Asynchronous
+    public Future<Author> createAuthor(final String name, final String surname) {
         final Author author = new Author(UUID.randomUUID(), name, surname);
         em.persist(author);
-        return author;
+        return new AsyncResult<>(author);
     }
 
     public Author getAuthor(final String id) {
